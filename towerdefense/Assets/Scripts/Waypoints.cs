0739c48103df47213aed6ff24c3529a4a98bd1ee
veketable <veikka.mikkola@gmail.com>
2021-11-09 14:33:06 +0200

    vihut liikkuu random reittejä pitkin

--- towerdefense/Assets/Scripts/Waypoints.cs
+++ towerdefense/Assets/Scripts/Waypoints.cs
@@ -4,31 +4,63 @@ using UnityEngine;
 
 public class Waypoints : MonoBehaviour
 {
-    private GameObject[] waypoints = new GameObject[4];
+    private GameObject[] waypoints = new GameObject[9];
     int current = 0;
     float rotSpeed;
     public float speed;
     float WPradius = 1;
+    bool alive = true;
     // Start is called before the first frame update
     void Start()
     {
-        waypoints[0] = GameObject.Find("Spawn");
-        waypoints[1] = GameObject.Find("Turn 1");
-        waypoints[2] = GameObject.Find("Turn 2");
-        waypoints[3] = GameObject.Find("Finish");
+        current = Random.Range(0, 3);
+        waypoints[0] = GameObject.Find("Turn 1 1");
+        waypoints[1] = GameObject.Find("Turn 1 2");
+        waypoints[2] = GameObject.Find("Turn 1 3");
+
+        waypoints[3] = GameObject.Find("Turn 2 1");
+        waypoints[4] = GameObject.Find("Turn 2 2");
+        waypoints[5] = GameObject.Find("Turn 2 3");
+
+        waypoints[6] = GameObject.Find("Finish 1");
+        waypoints[7] = GameObject.Find("Finish 2");
+        waypoints[8] = GameObject.Find("Finish 3");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(waypoints[current].transform.position, transform.position) < WPradius)
+
+        if (Vector3.Distance(waypoints[current].transform.position, transform.position) < WPradius)
         {
-            current++;
+            
+            if (current <= 2 && current >= 0){
+                current = Random.Range(3,6);
+                Debug.Log(current);
+            }
+            else if(current <= 5 && current <= waypoints.Length){
+                current = Random.Range(6,9);
+                Debug.Log(current);
+            }
+            
             if(current >= waypoints.Length)
             {
                 Destroy(gameObject);
+                alive = false;
             }
         }
-        transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
+        if(alive == true)
+        {
+            Debug.Log(current);
+            Vector3 targetDir = waypoints[current].transform.position - transform.position;
+            float singleStep = speed * Time.deltaTime;
+            Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, singleStep, 0.0f);
+
+            transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
+            float angle = Vector3.Angle(targetDir, transform.right);
+            transform.rotation = Quaternion.LookRotation(newDir);
+            
+        }
+        
     }
 }

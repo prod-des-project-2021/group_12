7b469638d8055b7cadd94d90314561e44c3994f6
eerlit <flrepds@gmail.com>
2021-11-10 12:03:51 +0200

    parsittu tornien liike suhteessa vihollisiin

--- towerdefense/Assets/attackEnemy.cs
+++ towerdefense/Assets/attackEnemy.cs
@@ -4,29 +4,56 @@ using UnityEngine;
 
 public class attackEnemy : MonoBehaviour
 {
-    public List<GameObject> enemyList;
-    //public Transform enemy;
-    
-    //public GameObject test;
-    float distanceToEnemy;
-    float attackRange = 100;
+   
+   
+   
+    public float attackRange = 50;
+    public float turnSpeed = 10f;
+    private Transform target;
+    public Transform rotatingPart;
+    public string enemyTag = "mob";
     
     GameObject g;
 
 
-    private void OnTriggerEnter(Collider other) {
+    
+
+     private void attackEnemyOnRange(){
+
+
+    }
+    private void updateTarget(){
         
-        
-        if(other.TryGetComponent<Waypoints>(out Waypoints enemy)){
-            Debug.Log("osuu!");
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach(GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if(distanceToEnemy< shortestDistance){
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
         }
+        if(nearestEnemy != null && shortestDistance <= attackRange) {
+            {
+                target = nearestEnemy.transform;
+            }
+        }else{
+            target = null;
+        }
+
     }
 
-
+     private void OnDrawGizmosSelected() {
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
     // Start is called before the first frame update
     void Start()
     {
-     
+     InvokeRepeating("updateTarget",0f,0.5f);
         
     }
 
@@ -34,22 +61,15 @@ public class attackEnemy : MonoBehaviour
     void Update()
     {   
         
-
-       /* g = GameObject.FindGameObjectWithTag("mob");
-        if(g != null){
-            enemyList.Add(g);
-             Debug.Log(enemyList.Count);
-             g = null;
+        if(target == null)
+        {
+            return;
         }
-       
-        for (int i = 0; i < g.Length; i++){
-         distanceToEnemy = Vector3.Distance(transform.position, g[i].transform.position);
-         Debug.Log("halooooo"+ "lista "+g);
-          Debug.Log(distanceToEnemy);
-      if(distanceToEnemy < attackRange){
-                Debug.Log("attack rangella");
-                
-        }}*/
+      Vector3 direction = target.position - transform.position;
+      Quaternion lookRotation = Quaternion.LookRotation(direction);
+      Vector3 rotation = Quaternion.Lerp(rotatingPart.rotation,lookRotation, Time.deltaTime* turnSpeed).eulerAngles;
+      rotatingPart.rotation = Quaternion.Euler(0f,rotation.y, 0f);
+
        
         } 
         

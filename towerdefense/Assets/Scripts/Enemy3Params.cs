f6520f8814137a186ea8df3d95101e72ae6f9f3e
Juho Junnonaho <zzzzope@gmail.com>
2021-11-15 17:29:48 +0200

    mergetys tehty

---
+++ towerdefense/Assets/Scripts/Enemy3Params.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy3Params : MonoBehaviour
+{
+    [HideInInspector] public float speed = 35;
+    public float health = 150;
+    public float difficulty;
+    public static Enemy3Params enemy3ParamsInstance;
+    public static Enemy3Params enemy3HitInstance;
+
+    void Start()
+    {
+        enemy3HitInstance = this;
+        enemy3ParamsInstance = this;
+        difficulty = SpawnEnemy.spawnEnemyInstance.difficulty;
+        health = 150 * difficulty;
+        speed = 10 * difficulty;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void Enemy3NewInstance()
+    {
+        enemy3HitInstance =this;
+    }
+}

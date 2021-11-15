6cba9f52368d424af8da7b41cbc9477687372c75
Juho Junnonaho <zzzzope@gmail.com>
2021-11-15 15:13:40 +0200

    turrettien spawnauksen korjailua

--- towerdefense/Assets/Scripts/SpawnTurrets.cs
+++ towerdefense/Assets/Scripts/SpawnTurrets.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class SpawnTurrets : MonoBehaviour
 {
@@ -27,23 +28,41 @@ public class SpawnTurrets : MonoBehaviour
         SpawnAtMousePos();
     }
 
+
+
     private void SpawnAtMousePos()
     {
         GameObject turretToBuild = BuildManager.instance.getTurretToBuild();
 
         if (buildManager.getTurretToBuild() == null)
+        {
             return;
-        
+        }
 
         if(Input.GetMouseButtonDown(0))
         {
+
+            if(EventSystem.current.IsPointerOverGameObject())
+            {
+                return;
+            }
+                
+            
+
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             
 
-            if (Physics.Raycast(ray, out hit))
+            if (Physics.Raycast(ray, out hit) && hit.transform.tag == "Spawn area")
             {
+                Debug.Log(hit.transform.tag);
+                Debug.Log(hit.collider);
                 turret = Instantiate(turretToBuild, new Vector3(hit.point.x, hit.point.y + turret.transform.position.y, hit.point.z), Quaternion.identity);
+            }
+
+            else 
+            {
+                Debug.Log("Cant spawn tower here");
             }
         }
     }

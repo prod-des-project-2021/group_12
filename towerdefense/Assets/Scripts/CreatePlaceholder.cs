9407921cb974890d0a3ee128a2e361144a1b0840
Juho Junnonaho <zzzzope@gmail.com>
2021-11-25 00:31:38 +0200

    placeholderi turreteille paranneltu

--- towerdefense/Assets/Scripts/CreatePlaceholder.cs
+++ towerdefense/Assets/Scripts/CreatePlaceholder.cs
@@ -4,39 +4,40 @@ using UnityEngine;
 
 public class CreatePlaceholder : MonoBehaviour
 {
-
-    public GameObject tankPlaceHolder;
+    public GameObject placeHolder;
     private Vector3 spawnPos;
+  
     // Start is called before the first frame update
     void Start()
     {
-        spawnPos = tankPlaceHolder.transform.position;
+        spawnPos = placeHolder.transform.position;
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        TankPlaceHolder();   
+        PlaceHolder();
     }
 
-    public void TankPlaceHolder()
+    public void PlaceHolder()
     {
-        tankPlaceHolder.SetActive(true);
+        placeHolder.SetActive(true);
 
         Ray ray = Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1.0f));
         RaycastHit hit;
 
-        if(Physics.Raycast(ray, out hit))
+        if (Physics.Raycast(ray, out hit))
         {
             Vector3 newPos = hit.point;
-            tankPlaceHolder.transform.position = newPos;
+            placeHolder.transform.position = newPos;
         }
 
         if(Input.GetMouseButtonDown(0))
         {
-            tankPlaceHolder.SetActive(false);
-            tankPlaceHolder.transform.position = spawnPos;
-            
+            placeHolder.SetActive(false);
+            placeHolder.transform.position = spawnPos;
         }
     }
 }

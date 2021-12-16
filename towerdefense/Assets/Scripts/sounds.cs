b28a3f4cba480c4f525799de306ce770eacc4e88
veketable <veikka.mikkola@gmail.com>
2021-12-16 16:11:41 +0200

    lvlselectmenu

--- towerdefense/Assets/Scripts/sounds.cs
+++ towerdefense/Assets/Scripts/sounds.cs
@@ -17,110 +17,111 @@ public class sounds : MonoBehaviour
     public AudioSource sniperFireSound;
     public AudioSource zapTowerSound;
 
-public AudioSource enemyDeathSound;
+    public AudioSource enemyDeathSound;
 
-public AudioSource mainMenuBackgroundMusic;
-public AudioSource firstmapBackgroundMusic;
+    public AudioSource mainMenuBackgroundMusic;
+    public AudioSource firstmapBackgroundMusic;
 
 
-public void playTankFireSound()
-{
-tankFireSound.Play();
-}
-public void playMissileFireSound()
-{
- missileFireSound.Play();
-}
-public void playMissileExplosionSound(){
+    public void playTankFireSound()
+    {
+            if (tankFireSound != null) tankFireSound.Play();
+    }
+    public void playMissileFireSound()
+    {
+            if (missileFireSound != null) missileFireSound.Play();
+    }
+    public void playMissileExplosionSound(){
+
+            if (missileHitSound != null) missileHitSound.Play();
+    }
+    public void playMinigunSpinSound(){
+            if (minigunSpinSound != null) minigunSpinSound.Play();
+    }
+    public void playMinigunFireSound()
+    {
+            if (minigunFireSound != null) minigunFireSound.Play();
+    }
+    public void playSniperFireSound()
+    {
+            if (sniperFireSound != null) sniperFireSound.Play();
+    }
+    public void playZapTowerFireSound()
+    {
+            if (zapTowerSound != null) zapTowerSound.Play();
+    }
+    public void playEnemyDeathSound()
+    {
+            if(enemyDeathSound != null) enemyDeathSound.Play();
+
+        }
+    int count = 0;
+    IEnumerator fadeInMainmenuMusic;
+    private void Start() {
+
+     soundInstance = this;
+    }
+
     
-    missileHitSound.Play();
-}
-public void playMinigunSpinSound(){
-    minigunSpinSound.Play();
-}
-public void playMinigunFireSound()
-{
-     minigunFireSound.Play();
-}
-public void playSniperFireSound()
-{
-    sniperFireSound.Play();
-}
-public void playZapTowerFireSound()
-{
-    zapTowerSound.Play();
-}
-public void playEnemyDeathSound()
-{
-    enemyDeathSound.Play();
-}
-int count = 0;
-IEnumerator fadeInMainmenuMusic;
-private void Start() {
 
- soundInstance = this;
-}
-
-    
-
-public static IEnumerator fadeOut ( AudioSource audioSource, float fadeTime) {
-    {
-        float startVolume = audioSource.volume;
-        Debug.Log("vola " +audioSource.volume);
-        while (audioSource.volume > 0){
-            audioSource.volume -= startVolume * Time.deltaTime /fadeTime;
-             Debug.Log("vola2 " +audioSource.volume);
-            yield return null;
+    public static IEnumerator fadeOut ( AudioSource audioSource, float fadeTime) {
+        {
+            float startVolume = audioSource.volume;
+            Debug.Log("vola " +audioSource.volume);
+            while (audioSource.volume > 0){
+                audioSource.volume -= startVolume * Time.deltaTime /fadeTime;
+                 Debug.Log("vola2 " +audioSource.volume);
+                yield return null;
+            }
+            audioSource.Stop();
+            audioSource.volume = startVolume;
         }
-        audioSource.Stop();
-        audioSource.volume = startVolume;
     }
-}
-public static IEnumerator fadeIn ( AudioSource audioSource, float fadeTime) {
-    {
-        audioSource.Play();
+    public static IEnumerator fadeIn ( AudioSource audioSource, float fadeTime) {
+        {
+            audioSource.Play();
         
-        float startVolume = audioSource.volume;
-        while (audioSource.volume < 1){
+            float startVolume = audioSource.volume;
+            while (audioSource.volume < 1){
             
-            audioSource.volume += startVolume + Time.deltaTime /fadeTime;
+                audioSource.volume += startVolume + Time.deltaTime /fadeTime;
             
-            yield return null;
+                yield return null;
+            }
+        
+           // audioSource.volume = startVolume;
         }
-        
-       // audioSource.volume = startVolume;
     }
-}
- private void Update() 
-    {
-        if(SceneManager.GetActiveScene().name == "MainMenu"){
+     private void Update() 
+        {
+            if(SceneManager.GetActiveScene().name == "MainMenu"){
            
-           if(!mainMenuBackgroundMusic.isPlaying){
-           fadeInMainmenuMusic = fadeIn(mainMenuBackgroundMusic, 3f);
-           StartCoroutine(fadeInMainmenuMusic);
-           }
-           if(SceneSwitcher.switcherInstance.fadeToNextLevelStarted && count == 0)
-    {
-        StopCoroutine(fadeInMainmenuMusic);
-        IEnumerator fadeOutMainmenuMusic = fadeOut(mainMenuBackgroundMusic, 1f);
-        StartCoroutine(fadeOutMainmenuMusic);
-         count++;
-    }
-           
-        }
-        
-        if(SceneManager.GetActiveScene().name == "FirstMap"){
-
-               if(!firstmapBackgroundMusic.isPlaying)
-               {  
-            IEnumerator fadeInFirstMapMusic = fadeIn(firstmapBackgroundMusic, 3f);
-            
-            StartCoroutine(fadeInFirstMapMusic);
+               if(!mainMenuBackgroundMusic.isPlaying){
+               fadeInMainmenuMusic = fadeIn(mainMenuBackgroundMusic, 3f);
+               StartCoroutine(fadeInMainmenuMusic);
                }
-            
+               if(SceneSwitcher.switcherInstance.fadeToNextLevelStarted && count == 0)
+        {
+            StopCoroutine(fadeInMainmenuMusic);
+            IEnumerator fadeOutMainmenuMusic = fadeOut(mainMenuBackgroundMusic, 1f);
+            StartCoroutine(fadeOutMainmenuMusic);
+             count++;
         }
+           
+            }
+        
+            if(SceneManager.GetActiveScene().name == "FirstMap"){
 
-    }
+                   if(!firstmapBackgroundMusic.isPlaying)
+                   {  
+                IEnumerator fadeInFirstMapMusic = fadeIn(firstmapBackgroundMusic, 3f);
+            
+                StartCoroutine(fadeInFirstMapMusic);
+                   }
+            
+            }
+
+        }
 
 
 }
